9d097a8a2023465f8f04bc1b0a1defb4d42c2532
pavithraalamelu <pavithrasivasamy22@gmail.com>
2023-07-03 06:49:27 +0530

    first commit

---
+++ MUSIC PLAYER/Music.js
@@ -0,0 +1,44 @@
+let progress=document.getElementById("progress");
+let song=document.getElementById("song");
+let ctrlicon=document.getElementById("controlIcon");
+
+
+
+song.onloadedmetadata=function  () {
+  
+    progress.max=song.duration;
+    progress.value=song.currentTime;
+}
+function playpause(){
+    if(ctrlicon.classList.contains("fa-pause")){
+song.pause();
+ctrlicon.classList.remove("fa-pause");
+ctrlicon.classList.add("fa-play")
+
+    }
+    else{
+        song.play();
+        ctrlicon.classList.add("fa-pause");
+ctrlicon.classList.remove("fa-play");
+
+    }
+}
+if(song.play()){
+    setInterval(() => {
+       progress.value=song.currentTime; 
+    }, 500);
+}
+progress.onchange=function () {
+    song.play();
+    song.currentTime=progress.value;
+    ctrlicon.classList.add("fa-pause");
+ctrlicon.classList.remove("fa-play");
+
+}
+function backward(){
+    alert("cannot backward");
+    
+}
+function  forward() {
+alert("cannot forward");
+}
